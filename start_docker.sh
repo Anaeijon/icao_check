aeec7e1c59721df483473a668628958aa5b81ed4
Anaeijon <david.doering@knopf-und-wolle.de>
2018-06-25 16:32:25 +0200

    automatic backup of changes on git container

--- start_docker.sh
+++ start_docker.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+docker load -i ${PWD}/docker_backups/medsecteam5.tar.gz
 
 # start container with current directory mounted as home:
 docker run -v ${PWD}:/home/medsecteam5 --name mst5 -p 5001:5001 -dit medsecteam5/medsecteam5:base
 
 # get a shell in the container:
-# simple: 
+# simple:
 #   docker exec -u medsecteam5 -it mst5 /bin/bash
 # as user in home:
 docker exec -u medsecteam5 -it mst5 /bin/bash -c 'cd ~; exec "${SHELL:-sh}"'
 
+# commit changes to base image
+docker commit --author medsecteam5 mst5 medsecteam5/medsecteam5:base
+# export docker edited docker image
+docker save medsecteam5/medsecteam5:base | gzip -c > ${PWD}/docker_backups/medsecteam5.tar.gz
