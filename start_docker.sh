993c30830ace4b5daaf4f08e77481c81877929c4
Anaeijon <david.doering@knopf-und-wolle.de>
2018-06-25 16:52:25 +0200

    mehr docker

--- start_docker.sh
+++ start_docker.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
-docker load -i ${PWD}/docker_backups/medsecteam5.tar.gz
+read -r -p "Load image from file? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    docker load -i ${PWD}/docker_backups/medsecteam5.tar.gz
+fi
 
 # start container with current directory mounted as home:
-docker run -v ${PWD}:/home/medsecteam5 --name mst5 -p 5001:5001 -dit medsecteam5/medsecteam5:base
+read -r -p "Run image as container 'mst5'? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    docker run -v ${PWD}:/home/medsecteam5 --name mst5 -p 5001:5001 -dit medsecteam5/medsecteam5:base
+fi
 
 # get a shell in the container:
 # simple:
 #   docker exec -u medsecteam5 -it mst5 /bin/bash
 # as user in home:
-docker exec -u medsecteam5 -it mst5 /bin/bash -c 'cd ~; exec "${SHELL:-sh}"'
+read -r -p "Get interactive Shell? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    docker exec -u medsecteam5 -it mst5 /bin/bash -c 'cd ~; exec "${SHELL:-sh}"'
+fi
 
-# commit changes to base image
-docker commit --author medsecteam5 mst5 medsecteam5/medsecteam5:base
-# export docker edited docker image
-docker save medsecteam5/medsecteam5:base | gzip -c > ${PWD}/docker_backups/medsecteam5.tar.gz
+read -r -p "Commit changes on Container to image and backup? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    # commit changes to base image
+    docker commit --author medsecteam5 mst5 medsecteam5/medsecteam5:base
+    # export docker edited docker image
+    docker save medsecteam5/medsecteam5:base | gzip -c > ${PWD}/docker_backups/medsecteam5.tar.gz
+fi
+
+read -r -p "Stop container? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    docker container stop mst5
+fi
+
+read -r -p "remove? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    docker container rm mst5
+fi
